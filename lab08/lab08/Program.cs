8dbe023f662e0a44ad42d5da8f085511e79aa463
minwichita <59030206@kmitl.ac.th>
2017-10-20 20:03:54 +0700

    edmin-2.1.2-1

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,6 +1,4 @@
-﻿
-using System;
-
+﻿using System;
 namespace ConsoleAppArray
 {
     class Program
@@ -8,28 +6,15 @@ namespace ConsoleAppArray
         static void Main(string[] args)
         {
             int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            int temp = 0;
+            for (int i = 0; i < data.Length; i++) { data[i] = i; }
+            Console.WriteLine("data[7] : " + data[7]);
+            Console.WriteLine("data[4] + data[9] : " + (data[4] + data[9]));
+            Console.WriteLine("data[2] == data[6] ? : " + (data[2] == data[6] ? "equal" : "not equal"));
+            for (int i = 0; i < data.Length; i++) { temp += data[i]; }
+            Console.WriteLine("Sum : " + temp);
+            Console.Write("All values : ");
+            for (int i = 0; i < data.Length; i++) { Console.Write(data[i] + " "); }
             Console.ReadLine();
         }
     }
