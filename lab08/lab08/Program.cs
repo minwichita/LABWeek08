61e854e59e382021fc1178582aca3301231d3621
minwichita <59030206@kmitl.ac.th>
2017-10-20 20:11:34 +0700

    edmin-2.1.4-2

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,36 +1,104 @@
-﻿
-using System;
-
+﻿using System;
 namespace ConsoleAppArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
-            Console.ReadLine();
+            int[] firstMonth = new int[12];
+            int NumberOfMonth = 0;
+            string DayName = "";
+            firstMonth[0] = 0;    //January 
+            firstMonth[1] = 3;    //February
+            firstMonth[2] = 3;    //March 
+            firstMonth[3] = 6;    //April  
+            firstMonth[4] = 1;    //May  
+            firstMonth[5] = 4;    //June
+            firstMonth[6] = 6;    //July
+            firstMonth[7] = 2;    //August
+            firstMonth[8] = 5;    //September
+            firstMonth[9] = 0;    //October
+            firstMonth[10] = 3;   //November
+            firstMonth[11] = 5;   //December
+            Console.Write("Enter date : ");
+            int date = int.Parse(Console.ReadLine());
+            if (date >= 32)
+            {
+                Console.WriteLine("Error!, Please enter numbers 00 to 31 only.");
+                goto lineOUT;
+            }
+            Console.Write("Enter month : ");
+            int month = int.Parse(Console.ReadLine());
+            switch (month)
+            {
+                case 01:
+                    NumberOfMonth = firstMonth[0];
+                    break;
+                case 02:
+                    NumberOfMonth = firstMonth[1];
+                    break;
+                case 03:
+                    NumberOfMonth = firstMonth[2];
+                    break;
+                case 04:
+                    NumberOfMonth = firstMonth[3];
+                    break;
+                case 05:
+                    NumberOfMonth = firstMonth[4];
+                    break;
+                case 06:
+                    NumberOfMonth = firstMonth[5];
+                    break;
+                case 07:
+                    NumberOfMonth = firstMonth[6];
+                    break;
+                case 08:
+                    NumberOfMonth = firstMonth[7];
+                    break;
+                case 09:
+                    NumberOfMonth = firstMonth[8];
+                    break;
+                case 10:
+                    NumberOfMonth = firstMonth[9];
+                    break;
+                case 11:
+                    NumberOfMonth = firstMonth[10];
+                    break;
+                case 12:
+                    NumberOfMonth = firstMonth[11];
+                    break;
+                default:
+                    Console.WriteLine("Error!, Please enter numbers 00 to 12 only.");
+                    goto lineOUT;
+            }
+            int Ans = (date + NumberOfMonth - 1) % 7;
+            switch (Ans)
+            {
+                case 0:
+                    DayName = "Sunday";
+                    break;
+                case 1:
+                    DayName = "Monday";
+                    break;
+                case 2:
+                    DayName = "Tuesday";
+                    break;
+                case 3:
+                    DayName = "Wednesday";
+                    break;
+                case 4:
+                    DayName = "Thursday";
+                    break;
+                case 5:
+                    DayName = "Friday";
+                    break;
+                case 6:
+                    DayName = "Saturday";
+                    break;
+            }
+            Console.WriteLine("{0}/{1}/2560 is {2}.", date, month, DayName);
+            lineOUT:
+            Console.ReadKey();
         }
     }
 }
