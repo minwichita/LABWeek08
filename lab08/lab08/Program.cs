dce0941641034f05c9fc9c1a5234cbf5275969ea
minwichita <59030206@kmitl.ac.th>
2017-10-20 20:09:36 +0700

    edmin-2.1.4-1

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,36 +1,26 @@
-﻿
-using System;
-
+﻿using System;
 namespace ConsoleAppArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
-            Console.ReadLine();
+            double[] GPS = new double[8];
+            double GPA = 0;
+            for (int i = 0; i < GPS.Length; i++)
+            {
+                Console.Write("Enter grade on semester {0} : ", i + 1);
+                GPS[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("--------------a---------------");
+            for (int i = 0; i < GPS.Length; i++)
+            {
+                Console.WriteLine("Grade on semester {0} : {1}", i + 1, GPS[i]);
+            }
+            Console.WriteLine("--------------b---------------");
+            for (int i = 0; i < GPS.Length; i++) { GPA += GPS[i]; }
+            Console.WriteLine("GPA is : " + GPA / GPS.Length);
+            Console.ReadKey();
         }
     }
 }
