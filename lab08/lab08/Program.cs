94169a676a7e7e5792230223c9e5d2faad25f50f
minwichita <59030206@kmitl.ac.th>
2017-10-20 20:57:15 +0700

    edmin-2.3.1-2

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,35 +1,28 @@
-﻿
-using System;
+﻿using System;
 
 namespace ConsoleAppArray
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            int[,] multiplyTable = new int[12, 12];
+            for (int i = 0, table = 2; i < multiplyTable.GetLength(0); i++, table++)
+            {
+                for (int j = 0; j < multiplyTable.GetLength(1); j++)
+                {
+                    multiplyTable[j, i] = table * (j + 1);
+                }
+            }
+            for (int row = 0; row < multiplyTable.GetLength(0); row++)
+            {
+                for (int col = 0; col < multiplyTable.GetLength(1); col++)
+                {
+                    Console.Write("{0,5}", multiplyTable[row, col]);
+                }
+                Console.WriteLine();
+            }
             Console.ReadLine();
         }
     }
