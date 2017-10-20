86bc3942e8a3ad629e9d929acfc23795bd363508
minwichita <59030206@kmitl.ac.th>
2017-10-20 20:59:52 +0700

    edmin-2.4.1-1

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,36 +1,18 @@
-﻿
-using System;
-
-namespace ConsoleAppArray
+﻿using System;
+namespace ArrayOfArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
-            Console.ReadLine();
+            string[][] jagged = new string[2][];
+            jagged[0] = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            jagged[1] = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+            Console.WriteLine("Day_names of week : " + (string.Join(",", jagged[0])));
+            Console.WriteLine("Month_names of year : " + (string.Join(",", jagged[1])));
+            Console.ReadKey();
         }
     }
 }
+
