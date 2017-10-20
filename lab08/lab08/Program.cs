cc952012beb917748e08c5ffe580113b2d3d1f69
minwichita <59030206@kmitl.ac.th>
2017-10-20 21:05:16 +0700

    edmin-3.2.2

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,35 +1,31 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleAppArray
+namespace QueueDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            Queue<string> queue1 = new Queue<string>();
+            queue1.Enqueue("Red");
+            queue1.Enqueue("Green");
+            queue1.Enqueue("Blue");
+            queue1.Enqueue("Orange");
+            queue1.Enqueue("Yellow");
+            Console.WriteLine("The elements in the queue are:");
+            foreach (string s in queue1)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("The element Red is contain in the queue:"
+ + queue1.Contains("Red"));
+            Console.WriteLine("The element Black is contain in the queue:"
+ + queue1.Contains("Black"));
+            Console.WriteLine("The element Purple is contain in the queue:"
+ + queue1.Contains("Purple"));
+            Console.WriteLine("The element Green is contain in the queue:"
+ + queue1.Contains("Green"));
             Console.ReadLine();
         }
     }
