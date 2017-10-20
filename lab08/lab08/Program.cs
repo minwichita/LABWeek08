02434f77d1a849a2b27544d87e6e70df6b73bec1
minwichita <59030206@kmitl.ac.th>
2017-10-20 20:55:30 +0700

    edmin-2.3.1

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,35 +1,37 @@
 ﻿
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleAppArray
+namespace MultidimensionalArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            int[,] multiplyTable = new int[12, 12];
+            multiplyTable[0, 0] = 2 * 1;
+            multiplyTable[1, 0] = 2 * 2;
+            multiplyTable[2, 0] = 2 * 3;
+            multiplyTable[3, 0] = 2 * 4;
+            multiplyTable[4, 0] = 2 * 5;
+            multiplyTable[5, 0] = 2 * 6;
+            multiplyTable[6, 0] = 2 * 7;
+            multiplyTable[7, 0] = 2 * 8;
+            multiplyTable[8, 0] = 2 * 9;
+            multiplyTable[9, 0] = 2 * 10;
+            multiplyTable[10, 0] = 2 * 11;
+            multiplyTable[11, 0] = 2 * 12;
+            for (int row = 0; row < multiplyTable.GetLength(0); row++)
+            {
+                for (int col = 0; col < multiplyTable.GetLength(1); col++)
+                {
+                    Console.Write("{0,5}", multiplyTable[row, col]);
+                }
+                Console.WriteLine();
+            }
             Console.ReadLine();
         }
     }
