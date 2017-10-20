9c538aa88a4ab2889e6028d8351b43b0dbd2eb88
minwichita <59030206@kmitl.ac.th>
2017-10-20 21:08:44 +0700

    edmin-3.2.4

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,35 +1,30 @@
 ﻿
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace ConsoleAppArray
+namespace QueueDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            Queue<int> queue1 = new Queue<int>();
+            queue1.Enqueue(10);
+            queue1.Enqueue(20);
+            queue1.Enqueue(30);
+            queue1.Enqueue(40);
+            Console.WriteLine("The queue elements are:");
+            foreach (int i in queue1)
+            {
+                Console.WriteLine(i);
+            }
+            ArrayList array = new ArrayList(queue1.ToArray());
+            Console.WriteLine("\nContents of the copy");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadLine();
         }
     }
