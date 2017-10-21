6d135435e1f2fe4777323da52db094bae1e8caa6
minwichita <59030206@kmitl.ac.th>
2017-10-21 21:11:38 +0700

    edmin-3.3.2

--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,35 +1,33 @@
 ﻿
 using System;
+using System.Collections.Generic;
 
-namespace ConsoleAppArray
+namespace StackDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-            data[10] = 10;
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            Console.WriteLine(data[10]);
+            Stack<string> stack1 = new Stack<string>();
+            stack1.Push("************");
+            stack1.Push("RED");
+            stack1.Push("GREEN");
+            stack1.Push("BLUE");
+            stack1.Push("YELLOW");
+            stack1.Push("***********");
+            stack1.Push("** COLOR **");
+            stack1.Push("***********");
+            Console.WriteLine("The elements in the stack1 are as:");
+            foreach (string s in stack1)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("The element RED contain in the stack "
++ stack1.Contains("RED"));
+            Console.WriteLine("The element YELLOW contain in the stack "
++ stack1.Contains("YELLOW"));
+            Console.WriteLine("The element BLACK contain in the stack "
++ stack1.Contains("BLACK"));
             Console.ReadLine();
         }
     }
